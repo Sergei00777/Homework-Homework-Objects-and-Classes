f102f52ee98ca1b63110afb759b6b6fd7d2eaaad
“Sergei007” <oni007@mail.ru>
2025-04-29 08:56:31 +0800

    второй коммит

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Домашняя Работа задача № 1
+# Домашняя Работа задача 2
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -7,6 +7,20 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        """Метод для оценки лекторов студентами."""
+        if (
+                isinstance(lecturer, Lecturer)
+                and course in lecturer.courses_attached
+                and course in self.courses_in_progress
+        ):
+            if course in lecturer.grades:
+                lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return "Ошибка"
 
 
 class Mentor:
@@ -18,14 +32,21 @@ class Mentor:
 
 class Lecturer(Mentor):
     """Класс для лекторов (наследуется от Mentor)."""
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
 
 
 class Reviewer(Mentor):
     """Класс для проверяющих (наследуется от Mentor)."""
 
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        """Метод для оценки домашних работ студентов."""
+        if (
+            isinstance(student, Student)
+            and course in self.courses_attached
+            and course in student.courses_in_progress
+        ):
             if course in student.grades:
                 student.grades[course].append(grade)
             else:
@@ -34,15 +55,21 @@ class Reviewer(Mentor):
             return "Ошибка"
 
 
+cool_lecturer = Lecturer("Ivan", "Petrov")
+cool_lecturer.courses_attached.append("Python")
+
+cool_reviewer = Reviewer("Anna", "Sidorova")
+cool_reviewer.courses_attached.append("Python")
 
 best_student = Student("Ruoy", "Eman", "male")
 best_student.courses_in_progress.append("Python")
-
-cool_reviewer = Reviewer("Some", "Buddy")
-cool_reviewer.courses_attached.append("Python")
 
 cool_reviewer.rate_hw(best_student, "Python", 10)
 cool_reviewer.rate_hw(best_student, "Python", 9)
 cool_reviewer.rate_hw(best_student, "Python", 8)
 
-print(best_student.grades)
+best_student.rate_lecturer(cool_lecturer, "Python", 10)
+best_student.rate_lecturer(cool_lecturer, "Python", 9)
+
+print("Оценки студента:", best_student.grades)
+print("Оценки лектора:", cool_lecturer.grades)
